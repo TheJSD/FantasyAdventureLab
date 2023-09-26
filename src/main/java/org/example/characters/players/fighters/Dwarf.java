9f1156d18c4aeb9fba9fdf0f166a9a6be1e8f27d
James Dickson <jamesstuartdickson@gmail.com>
2023-09-26 15:25:24 +0100

    Updated DwarfTest and Added Dwarf

---
+++ src/main/java/org/example/characters/players/fighters/Dwarf.java
@@ -0,0 +1,21 @@
+package org.example.characters.players.fighters;
+
+import org.example.characters.enemies.Enemy;
+import org.example.characters.equipment.fighterweapons.IWeapon;
+
+public class Dwarf extends Fighter{
+    private int drunkeness;
+    public Dwarf(int healthPoints, String name, IWeapon weapon) {
+        super(healthPoints, name, weapon);
+        this.drunkeness = 0;
+    }
+    public void drinkAle(){
+        this.gainHealth(30);
+        this.drunkeness += 25;
+    }
+
+    public int getDrunkeness() {
+        return drunkeness;
+    }
+}
+
--- src/test/java/fighters/DwarfTest.java
+++ src/test/java/fighters/DwarfTest.java
@@ -1,4 +1,54 @@
 package fighters;
 
+import org.example.characters.enemies.Goblin;
+import org.example.characters.equipment.fighterweapons.Axe;
+import org.example.characters.equipment.fighterweapons.Sword;
+import org.example.characters.players.fighters.Dwarf;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class DwarfTest {
+    Dwarf dwarf;
+    Sword sword;
+    Axe axe;
+    Goblin goblin;
+    @Before
+    public void before(){
+        sword = new Sword();
+        axe = new Axe();
+        dwarf = new Dwarf(200, "Grambles", sword);
+        goblin = new Goblin(70);
+    }
+    @Test
+    public void canGetName(){
+        assertEquals("Grambles", dwarf.getName());
+    }
+    @Test
+    public void canSwitchWeapon(){
+        dwarf.switchWeapon(axe);
+        assertEquals(axe, dwarf.getWeapon());
+    }
+    @Test
+    public void canAttackEnemy(){
+        dwarf.attack(goblin);
+        assertEquals(35, goblin.getHealthPoints());
+    }
+    @Test
+    public void canDrinkAle(){
+        dwarf.drinkAle();
+        assertEquals(25, dwarf.getDrunkeness());
+        assertEquals(230, dwarf.getHealthPoints());
+    }
+    @Test
+    public void canTakeDamage(){
+        dwarf.takeDamage(50);
+        assertEquals(150, dwarf.getHealthPoints());
+    }
+    @Test
+    public void canBeHealed(){
+        dwarf.gainHealth(20);
+        assertEquals(220, dwarf.getHealthPoints());
+    }
 }
